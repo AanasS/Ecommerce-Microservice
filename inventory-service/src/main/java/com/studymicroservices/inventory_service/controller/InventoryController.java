2b05a0320c0de0657a071150d12fcddd942ebf85
Ahmed Anas <ahmedanas951@gmail.com>
2025-03-23 13:00:52 +0200

    Remove target folder from repository

--- inventory-service/src/main/java/com/studymicroservices/inventory_service/controller/InventoryController.java
+++ inventory-service/src/main/java/com/studymicroservices/inventory_service/controller/InventoryController.java
@@ -1,18 +1,21 @@
 package com.studymicroservices.inventory_service.controller;
 
+import com.studymicroservices.inventory_service.dto.InventoryResponse;
 import com.studymicroservices.inventory_service.service.InventoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/inventory")
 @RequiredArgsConstructor
 public class InventoryController {
     private final InventoryService inventoryService;
-    @GetMapping("/{sku_code}")
+    @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public boolean isInStock(@PathVariable("sku_code") String skuCode){
-    return inventoryService.isInStock(skuCode);
+    public  List<InventoryResponse> isInStock(@RequestParam List<String> skuCode){
+        return inventoryService.isInStock(skuCode);
     }
 }
